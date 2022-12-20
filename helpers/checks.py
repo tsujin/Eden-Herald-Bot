2bf778029eeb4c614f4c2f674837e7934ae844e9
Justin <contact@tsujin.com>
2022-12-19 23:56:46 -0500

    fix owner environment variable

--- helpers/checks.py
+++ helpers/checks.py
@@ -7,6 +7,7 @@ Version: 5.4
 """
 
 import json
+import os
 from typing import Callable, TypeVar
 
 from discord.ext import commands
@@ -22,8 +23,7 @@ def is_owner() -> Callable[[T], T]:
     This is a custom check to see if the user executing the command is an owner of the bot.
     """
     async def predicate(context: commands.Context) -> bool:
-        with open("config.json") as file:
-            data = json.load(file)
+        data = json.loads(os.environ['HERALD_CONFIG'])
         if context.author.id not in data["owners"]:
             raise UserNotOwner
         return True
